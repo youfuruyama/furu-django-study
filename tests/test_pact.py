8a237cd8cfbc8eb5754c8412dba4e6df028bfe82
000001A008UFE <000001A008UFE@nsl.ad.nec.co.jp>
2019-01-31 17:31:22 +0900

    2019/01/31

---
+++ tests/test_pact.py
@@ -0,0 +1,11 @@
+import unittest
+
+from .pact_player import PactPlayer
+from controller import controller
+
+
+class TestPact(unittest.TestCase):
+    def test_hello(self):
+
+        api = controller.create_server()
+        PactPlayer('hello.json').run(self, api)
